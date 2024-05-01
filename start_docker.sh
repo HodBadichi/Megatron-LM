53d2e441c8772c9b8b4f47c1952bb139f5175cd9
HodBadihi <badihihod@gmail.com>
2024-05-01 11:13:15 +0000

    Fix start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it -v ~/Megatron-LM:/workspace/megatron megatron:version1
-
+docker run --gpus all --ipc=host --ulimit memlock=-1 --cap-add SYS_PTRACE --privileged --ulimit stack=67108864 -it -v ~/Megatron-LM:/workspace/megatron megatron:version1 bash -c "cd megatron && git config --global --add safe.directory /workspace/megatron"
