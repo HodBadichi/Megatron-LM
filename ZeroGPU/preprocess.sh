7d0c6da8ca08a2f761a5c5ee1e1e2a2745a6601e
root <root@instance-20240511-070545.us-central1-a.c.iucc-hpc-ai.internal>
2024-05-11 11:40:24 +0000

    changes

---
+++ ZeroGPU/preprocess.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pip install nltk
+pip install datasets
+
+python3 preprocess_helper.py
+
+
+
+python ../tools/preprocess_data.py \
+       --input wikipedia.json \
+       --output-prefix wikipedia \
+       --vocab-file vocab.json \
+       --tokenizer-type GPT2BPETokenizer \
+       --merge-file merges.txt \
+       --json-keys text \
+       --workers 32 \
+       --append-eod
